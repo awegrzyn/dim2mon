72d098c4a8d180a68bda2e519489835085ca9185
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2018-10-05 15:46:06 +0200

    Init

---
+++ cmake/FindDIM.cmake
@@ -0,0 +1,40 @@
+# - Try to find DIM
+# Once done this will define
+#  DIM_FOUND        - System has DIM
+#  DIM_INCLUDE_DIRS - The DIM include directories
+#  DIM_LIBRARIES    - The libraries needed to use DIM
+#
+# This script can use the following variables:
+#  DIM_ROOT - Installation root to tell this module where to look. (it tries /usr and /usr/local otherwise)
+
+find_package(PkgConfig)
+
+# find includes
+find_path(DIM_INCLUDE_DIR dim.h
+        HINTS ${DIM_ROOT} /usr/local/include /usr/include PATH_SUFFIXES "dim")
+# Remove the final "dim"
+get_filename_component(DIM_INCLUDE_DIR ${DIM_INCLUDE_DIR} DIRECTORY)
+
+# find libraries
+find_library(DIM_LIBRARY NAMES dim HINTS /usr/local/lib /usr/lib ${DIM_ROOT})
+
+set(DIM_LIBRARIES ${DIM_LIBRARY})
+set(DIM_INCLUDE_DIRS ${DIM_INCLUDE_DIR})
+
+include(FindPackageHandleStandardArgs)
+# handle the QUIETLY and REQUIRED arguments and set DIM_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args(DIM DEFAULT_MSG DIM_LIBRARY DIM_INCLUDE_DIR)
+
+mark_as_advanced(DIM_INCLUDE_DIR DIM_LIBRARY)
+
+if(DIM_FOUND)
+  # add target
+  if(NOT TARGET Dim::Dim)
+    add_library(Dim::Dim INTERFACE IMPORTED)
+    set_target_properties(Dim::Dim PROPERTIES
+      INTERFACE_INCLUDE_DIRECTORIES "${DIM_INCLUDE_DIRS}"
+      INTERFACE_LINK_LIBRARIES "${DIM_LIBRARY}"
+    )
+  endif()
+endif()
